593695249e8340991d7bc79643946ea0fff8d6bb
peteruche21 <anyaogupeter601@gmail.com>
2023-03-28 22:54:16 +0100

    update new task form UI

--- src/components/Form/Forms.tsx
+++ src/components/Form/Forms.tsx
@@ -11,6 +11,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import db from "../../db/polybase/sdk";
 import { generateId } from "../../utils/generateId";
 import moment from "moment";
+import DateButton from "../Date";
 
 const ProjectForm = ({ callback }: { callback: () => void }) => {
   const [{ wallet }] = useConnectWallet();
@@ -134,14 +135,14 @@ const TaskForm = ({ type, id, data, onUpdate }: ITaskFormProps) => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-md space-y-5">
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-md space-y-8">
       <div>
         {/* title */}
         <input
           type="text"
           id="title"
           placeholder="title"
-          className={`input input-bordered w-full max-w-md ${
+          className={`input w-full max-w-md ${
             errors.title && "border-red-500"
           }`}
           {...register("title", { required: true, value: data?.title })}
@@ -151,7 +152,7 @@ const TaskForm = ({ type, id, data, onUpdate }: ITaskFormProps) => {
       <textarea
         id="body"
         placeholder="description"
-        className={`textarea-bordered textarea textarea-lg h-[150px] w-full max-w-md ${
+        className={` textarea textarea-lg h-[150px] w-full max-w-md ${
           errors.description && "border-red-500"
         }`}
         {...register("description", {
@@ -161,25 +162,19 @@ const TaskForm = ({ type, id, data, onUpdate }: ITaskFormProps) => {
       />
 
       {/* date */}
-      <div className="inline-flex">
-        <span>from </span>
-        <div className="border border-gray-500 rounded-lg">
-          <DatePicker
-            selected={startDate}
-            onChange={(date: any) => setStartDate(moment(date).unix())}
-          />
+      <div className="flex justify-around">
+        <div className="inline-flex gap-3">
+          <span>from </span>
+          <DateButton value={startDate} setValue={setStartDate} />
         </div>
 
-        <span>to </span>
-        <div className="border border-gray-500 rounded-lg">
-          <DatePicker
-            selected={stopDate}
-            onChange={(date: any) => setStopDate(moment(date).unix())}
-          />
+        <div className="inline-flex gap-3">
+          <DateButton value={stopDate} setValue={setStopDate} />
+          <span>to </span>
         </div>
       </div>
 
-      <div className="inline-flex">
+      <div className="inline-flex justify-around w-full">
         {/* priority */}
         <Controller
           control={control}
@@ -194,6 +189,7 @@ const TaskForm = ({ type, id, data, onUpdate }: ITaskFormProps) => {
               className="select select-ghost"
               onChange={(val) => onChange(val.target.value)}
             >
+              <option>Priority</option>
               <option defaultValue="LOW" value="LOW">
                 LOW
               </option>
@@ -202,25 +198,37 @@ const TaskForm = ({ type, id, data, onUpdate }: ITaskFormProps) => {
             </select>
           )}
         />
-
-        {/* PR */}
+        {/* assignee */}
         <input
           type="text"
-          id="PR"
-          placeholder="PR"
-          className="input input-bordered w-full max-w-md"
-          {...register("PR", { value: data?.PR })}
+          id="assignee"
+          placeholder="assign"
+          className="input w-44"
+          {...register("assignee", { value: data?.assignee })}
         />
       </div>
 
-      {/* assignee */}
-      <input
-        type="text"
-        id="assignee"
-        placeholder="assign"
-        className="input input-bordered w-full max-w-md"
-        {...register("assignee", { value: data?.assignee })}
-      />
+      {/* PR */}
+      <div className="relative">
+        <svg
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+          className="w-5 h-5 absolute right-2 bottom-4"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+        >
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+        </svg>
+        <input
+          type="text"
+          id="PR"
+          placeholder="pull request"
+          className="input  w-full max-w-md"
+          {...register("PR", { value: data?.PR })}
+        />
+      </div>
 
       {createMutation.isLoading || updateMutation.isLoading ? (
         <button className="loading btn-primary btn-block btn"></button>
